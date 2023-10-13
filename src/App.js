161c0ad1bdb8fdb88a1b6b78fcd3033f5225f3b8
Demiladebdm <demiladefolarin@gmail.com>
2023-10-13 23:05:05 +0100

    blog client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Blog from "./pages/Blog/Blog";
+import Contact from "./pages/Contact/Contact";
+import Brokers from "./pages/Brokers/Brokers";
+import Mesh from "./pages/Mesh/Mesh";
+import Write from "./pages/Write/Write";
+import Register from "./pages/Authentication/Register";
+import Login from "./pages/Authentication/Login";
+import Chat from "./components/Chat/Chat";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/blog" element={<Blog />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/brokers" element={<Brokers />}></Route>
+          <Route path="/mesh" element={<Mesh />}></Route>
+          <Route path="/write" element={<Write />}></Route>
+
+
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+        </Routes>
+
+        <Chat />
+      </Router>
     </div>
   );
 }
